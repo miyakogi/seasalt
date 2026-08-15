4dd6dd3bac22e4f845ed4b48e48fb960f0353b2a
miyaco <miyako.dev@gmail.com>
2026-08-15 11:16:38 +0900

    feat: add nix flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "seasalt - fish-style autosuggestion and per-directory history for bash";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "seasalt";
+          version = "0.1.0";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+        };
+      });
+}
